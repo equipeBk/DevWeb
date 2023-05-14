4308b54295af18f0b97358427f8fcc66cb44a482
Michelly Alves <89709061+michelly-alves@users.noreply.github.com>
2023-05-14 00:19:38 -0300

    index.js

--- Alugueis de carro-MPA/index.js
+++ Alugueis de carro-MPA/index.js
@@ -39,43 +39,43 @@ app.post('/user/signin', async (req, res) => {
     let email = req.body.email
     const isEmailRegistered = await mongoRepository.isEmailAlreadyRegistered(email);
     if (isEmailRegistered) {
-        console.log(insertedUser)
-        console.log(email)
-        res.render('user/signin.ejs', { message: 'Esse email já está em uso' });
-    }else{
+      console.log(email)
+      res.render('user/signin.ejs', {
+        message: 'Esse email já está em uso'
+      });
+    } else {
       mongoRepository.saveUser(req.body).then((insertedUser) => {
         console.log('Inserted User')
         console.log(insertedUser)
         console.log(email)
         ////enviar email de confirmação
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'testeEstudosFaculdade@gmail.com',
-    pass: 'cybvmljdthjrzvkr'
-  }
-});
-const mailOptions = {
-  from: 'testeEstudosFaculdade@gmail.com',
-  to: req.body.email,
-  subject: 'Confirmação de cadastro',
-  text: `Olá ${req.body.name}, seu cadastro foi confirmado. Obrigado!`
-};
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('Email enviado: ' + info.response);
-  }
-});
-        res.redirect('/user/loja')
+        const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            /*fala cmg q eu mando a senha*/
+          }
+        });
+        const mailOptions = {
+          from: 'testeEstudosFaculdade@gmail.com',
+          to: req.body.email,
+          subject: 'Confirmação de cadastro',
+          text: `Olá ${req.body.name}, seu cadastro foi confirmado. Obrigado!`
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.error(error);
+          } else {
+            console.log('Email enviado: ' + info.response);
+          }
+        });
+        res.redirect('/')
       })
     }
   } catch (err) {
     console.error(err);
     res.sendStatus(500);
   }
-  
+
 });
 
 app.get('/user/signup', function (req, res) {
@@ -84,22 +84,69 @@ app.get('/user/signup', function (req, res) {
 });
 
 app.post('/user/signup', function (req, res) {
- 
+
   res.redirect('/loja/loja.ejs');
   console.log(" post user/signup")
 
-} )
+})
 app.get('/loja/loja', function (req, res) {
-  res.render('loja/loja.ejs');
-  console.log("get /loja/loja")
+  mongoRepository.getAllCarros().then((foundCarros) => {
+    res.render('loja/loja.ejs', {
+      carros: foundCarros,
+    })
+    console.log("get /loja/loja")
+  })
 });
 
-app.use('/user/signup', basicAuth( { authorizer: myAuthorizerMongo, authorizeAsync: true , challenge: true} ));
+////adicionar carro
+app.get('/admin/add-carro', function (req, res) {
+  res.render('admin/add-carro.ejs');
+  console.log(" admin/add-carro")
+});
+
+app.post('/add-carro', (req, res) => {
+  console.log('POST - /admin/add-carro')
+  let newCarro = req.body;
+  newCarro.createdBy = req.user;
+  console.log(newCarro)
+  mongoRepository.saveCarros(req.body).then((insertedCarro) => {
+    console.log('Inserted Carro')
+    console.log(insertedCarro)
+    res.redirect('admin/loja')
+  })
+})
+
+///deletar carro
+app.get('/deletar-carro', (req, res) => {
+  let deleteCarros = req._id // Obtém o ID do carro a ser excluído dos parâmetros de URL
+  mongoRepository.deleteCarros(deleteCarros)
+    .then(() => {
+      console.log(`Categoria com id ${deleteCarros} excluída com sucesso`)
+      res.redirect('admin/loja')
+    })
+})
+
+
+///loja admin
+app.get('/admin/loja', function (req, res) {
+  mongoRepository.getAllCarros().then((foundCarros) => {
+    res.render('admin/loja.ejs', {
+      carros: foundCarros,
+    })
+    console.log("get admin/loja")
+  })
+});
+
+app.use('/user/signup', basicAuth({
+  authorizer: myAuthorizerMongo,
+  authorizeAsync: true,
+  challenge: true
+}));
 
 function myAuthorizerMongo(email, password, cb) {
-    console.log(database.getUsers(email, password).then(users => {
-        return cb(null, users.length > 0);
-    }));
+  console.log(database.getUsers(email, password).then(users => {
+    return cb(null, users.length > 0);
+  }));
 }
 
 app.get('/user/signup', (req, res) => {
@@ -110,8 +157,28 @@ app.get('/user/signup', (req, res) => {
   console.log('Password: ' + password);
 });
 
+///buscar carros
+app.post('/busca', (req, res) => {
+  const nome = req.body.busca.toLowerCase(); // converte o nome para minúsculo
+  const marca = req.body.busca.toLowerCase(); // converte a marca para minúsculo
   
-  
+  mongoRepository.getAllCarros()
+    .then(carros => {
+      const carrosEncontrados = carros.filter(carro => 
+        carro.nome.toLowerCase().includes(nome) && 
+        carro.marca.toLowerCase().includes(marca)
+      );
+      
+      res.render('loja/loja.ejs', { carros: carrosEncontrados });
+    })
+    .catch(err => {
+      console.error(`Erro ao buscar carros: ${err.message}`);
+      res.redirect('loja/loja');
+    });
+});
+
+
+
 
 /*
     app.get('/list', (req, res) => {
@@ -206,6 +273,6 @@ app.get('/user/signup', (req, res) => {
         })
     })
     */
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
