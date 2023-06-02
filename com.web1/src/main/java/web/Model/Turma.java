8968529b08d4e7b4aca9178b597a46b4961e50cb
José Wytalo <112438631+JwapWytalo@users.noreply.github.com>
2023-06-01 23:50:05 -0300

    lab07 quase pronto

--- com.web1/src/main/java/web/Model/Turma.java
+++ com.web1/src/main/java/web/Model/Turma.java
@@ -3,34 +3,54 @@ package web.Model;
 import java.util.List;
 
 public class Turma {
+
+	Aluno aluno = new Aluno();
+
 	int codigo;
 	String disciplina;
 	int semestre;
 	List<Aluno> alunos;
-	
-	//gets and sets
-	
+
+	public Turma(int codigo, String disciplina, int semestre, List<Aluno> aluno) {
+		this.codigo = codigo;
+		this.disciplina = disciplina;
+		this.semestre = semestre;
+		this.alunos = aluno;
+	}
+
+	public Turma() {
+	}
+
+	// gets and sets
+
 	public int getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getDisciplina() {
 		return disciplina;
 	}
+
 	public void setDisciplina(String disciplina) {
 		this.disciplina = disciplina;
 	}
+
 	public int getSemestre() {
 		return semestre;
 	}
+
 	public void setSemestre(int semestre) {
 		this.semestre = semestre;
 	}
+
 	public List<Aluno> getAlunos() {
 		return alunos;
 	}
+
 	public void setAlunos(List<Aluno> alunos) {
 		this.alunos = alunos;
 	}
--- com.web1/src/main/java/web/Rest/Controller.java
+++ com.web1/src/main/java/web/Rest/Controller.java
@@ -1,24 +1,69 @@
 package web.Rest;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import web.Service.AlunoService;
+import web.Service.TurmaService;
+import web.Model.Aluno;
+import web.Model.Turma;
 
 @RestController
 public class Controller {
-	
+
 	@Autowired
-    AlunoService alunoService;
+	TurmaService turmaService;
 
-   
-    @GetMapping
-    List<Aluno> getNome(@RequestParam Map<String, String> params){
+	@GetMapping("/api/turma")
+	public List<Turma> getTurmas(@RequestParam Map<String, String> params) {
+		return turmaService.getTurma();
+	}
 
-        return alunoService.getNome(buscaNome);
-    }
+	@GetMapping("api/turma/{codigo}")
+
+	public Turma getTurmaByCodigo(@PathVariable int codigo) {
+		return turmaService.getTurmaByCodigo(codigo);
+	}
+	
+	@PostMapping("/api/turma")
+	public void addTurma(@RequestBody Turma turma) {
+	    turmaService.addTurma(turma.getDisciplina(), turma.getSemestre(), turma.getAlunos());
+	}
+	
+	@PutMapping("/api/turma/{codigo}")
+	public void editarTurma(@PathVariable int codigo, @RequestBody Turma turmaEditar) {
+	    Turma turmaExistente = turmaService.getTurmaByCodigo(codigo);
+	    
+	    if (turmaExistente != null) {
+	        turmaExistente.setDisciplina(turmaEditar.getDisciplina());
+	        turmaExistente.setSemestre(turmaEditar.getSemestre());
+	        turmaExistente.setAlunos(turmaEditar.getAlunos());
+	        
+	    }
+	}
+	
+	@DeleteMapping("/api/turma/{codigo}")
+	public void deletarTurma(@PathVariable int codigo) {
+	    Turma turma = turmaService.getTurmaByCodigo(codigo);
+	    
+	    if (turma != null) {
+	        turmaService.removerTurma(turma);
+	        
+	    }
+	}
+	
+	@GetMapping("/api/turma/{codigo}/alunos")
+	public List<Aluno> getAlunosByTurmaCodigo(@PathVariable int codigo) {
+	    return turmaService.getAlunosByTurmaCodigo(codigo);
+	}
 
 }
-
